6c9a178c5442e8d1c944743542f731d079a4fde1
11_Fa'iz Zaki Sidiq <zaki.tkjsmeksa25@gmail.com>
2026-09-01 21:24:00 +0700

    modul_geometri

---
+++ modul_geometri.py
@@ -0,0 +1,11 @@
+def luas_segiempat(s):
+    return s * s
+
+def keliling_segiempat(s):
+    return 4 * s
+
+def luas_bundaran(r):
+    return 3.14 * r * r
+
+def luas_tiga_sisi(a, t):
+    return 0.5 * a * t
